c83b914518d5e00efde5280043c25f2669370830
BAPATLA SRAVAN <103932090+bapatlasravan@users.noreply.github.com>
2022-08-04 12:03:41 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.ACCESS_MODIFIERS
+{
+    class program
+    { 
+        internal void BAPATLA()
+        {
+        Console.WriteLine("HELLO BAPATLA.......");
+        }
+        public void SRAVAN()
+        {
+            Console.WriteLine("HELLO SRAVAN......");
+        }
+        private void RAVAN()
+        { 
+            Console.WriteLine("HELLO RAVAN.......");
+        }
+        protected void hello()
+        {
+            Console.WriteLine("hello.............");
+        }
+        protected internal void world()
+        {
+            Console.WriteLine("world...........");
+        }
+        static void Main(string[] args)
+        {
+            program obj = new program();
+            obj.hello();
+            obj.world();
+            obj.SRAVAN();
+            obj.RAVAN();
+            obj.BAPATLA();
+        }
+    }
+}
